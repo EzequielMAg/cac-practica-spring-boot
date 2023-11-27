11efa0f6a9bfd69e90f6bbdb8a2c0a5b07ece7f7
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-11-26 23:56:29 -0300

    coment

--- src/main/java/com/cac/practicaspringboot/models/User.java
+++ src/main/java/com/cac/practicaspringboot/models/User.java
@@ -1,5 +1,7 @@
 package com.cac.practicaspringboot.models;
 
+// La siguiente anotacion, no va a funcionar aun porque debemos importar la libreria de JPA/HIBERNATE
+// Hibernate con esta anotacion crea la tabla segun la clase User.
 //@Entity
 public class User {
 
