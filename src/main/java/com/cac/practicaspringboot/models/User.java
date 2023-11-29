2b31b9f0e399e0a9460b8ac654235689efbd1a39
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2023-11-29 12:57:48 -0300

    Implementa JPA-Hibernate - Conexion a la BD - Mapeo de la clase User en la BD

--- src/main/java/com/cac/practicaspringboot/models/User.java
+++ src/main/java/com/cac/practicaspringboot/models/User.java
@@ -1,9 +1,27 @@
 package com.cac.practicaspringboot.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 // La siguiente anotacion, no va a funcionar aun porque debemos importar la libreria de JPA/HIBERNATE
 // Hibernate con esta anotacion crea la tabla segun la clase User.
-//@Entity
+@Entity
+@Table(name = "usuarios")
+@Getter     // Esta anotacion como la sig, va a generar todos los getters y setters por c/ atributo, gracias a lombok
+@Setter
+
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id;
+
+    // en este atributo no use la anotacion @Column por lo tanto, hibernate va a crear la columna con el mismo nombre
+    private String email;
+
+    @Column(name = "contraseña")
+    private String password;
 
 }
--- src/main/java/com/cac/practicaspringboot/repositories/UserRepository.java
+++ src/main/java/com/cac/practicaspringboot/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.cac.practicaspringboot.repositories;
 
+import com.cac.practicaspringboot.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
 }
